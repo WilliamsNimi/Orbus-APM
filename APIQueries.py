b226bb3aa14407501dcac352ddcada33dc7a6b8e
Nimi Williams <williamson.nimi@gmail.com>
2024-11-17 20:54:16 +0000

    Replaced the simulation endpoints with the correct endpoints

--- APIQueries.py
+++ APIQueries.py
@@ -1,80 +1,97 @@
 import requests
 import json
 
-""" End points
+#End points
 getObjectsURL = "https://sterlingbank-api.iserver365.com/odata/Objects"
-getObjectAttributes = "https://sterlingbank-api.iserver365.com/odata/Objects("#{key})/AttributeValues
-patchObject = "https://sterlingbank-api.iserver365.com/odata/Objects("#{key})
-apmServices = ""
-"""
+getObjectAttributesURL = "https://sterlingbank-api.iserver365.com/odata/Objects("
+patchObjectURL = "https://sterlingbank-api.iserver365.com/odata/Objects("
+APMServicesURL = "http://10.0.20.196:3202/api/metrics/summary?q={%20resource.service.name%20!%3D%20%22%20%22%20}&groupBy=resource.service.name"
 
+"""
 #Simulated RESTAPI endpoints for Orbus and APM
 getObjectsfakeURL = "http://127.0.0.1:5000/odata/Objects"
 getObjectAttributesfakeURL = "http://127.0.0.1:5000/odata/Objects("
 patchObjectfakeURL = "http://127.0.0.1:5000/odata/Objects("
 APMServicesFakeURL = "http://127.0.0.1:5000/grafana/services/SLO"
+"""
 
 def getObjects():
-    #TODO: Replace URL with correct URL from ORbus
-    objectsRes = requests.get(getObjectsfakeURL)
-    if objectsRes.status_code == 200:
-        return objectsRes.content.decode('utf-8')
+    try:
+        objectsRes = requests.get(getObjectsURL)
+        if objectsRes.status_code == 200:
+            return objectsRes.content.decode('utf-8')
+    except Exception as e:
+        print(e)
 
 
 def getObjectAttributes(objectID):
-    #TODO: Replace URL with correct URL from ORbus
-    URL = getObjectAttributesfakeURL + str(objectID) + ")/AttributeValues"
-    attributes = requests.get(URL)
-    if attributes.status_code == 200:
-        return attributes.content.decode('utf-8')
+    try:
+        URL = getObjectAttributesURL + str(objectID) + ")/AttributeValues"
+        attributes = requests.get(URL)
+        if attributes.status_code == 200:
+            return attributes.content.decode('utf-8')
+    except Exception as e:
+        print(e)
     
 def patchObject(objectID, metric_val):
-    #TODO: replace URL with correct Orbus Patch URL
-    URL = patchObjectfakeURL + str(objectID) + ")"
-    attributeChangePattern = { 
-        "AttributeValuesFlat":
-        {
-            'Availability Score': 5 
+    try:
+        URL = patchObjectURL + str(objectID) + ")"
+        attributeChangePattern = { 
+            "AttributeValuesFlat":
+            {
+                'Availability Score': 5 
+            }
         }
-    }
-    attributeChangePattern['AttributeValuesFlat']['Availability Score'] = metric_val
+        attributeChangePattern['AttributeValuesFlat']['Availability Score'] = metric_val
 
-    req = requests.patch(URL, attributeChangePattern)
+        req = requests.patch(URL, attributeChangePattern)
 
-    return req
+        return req.status_code
+    except Exception as e:
+        print(e)
 
 def getServicesSLO():
-    #TODO: replace URL with correct endpoint from APM
-    servicesSLO = requests.get(APMServicesFakeURL)
-    if servicesSLO.status_code == 200:
-        return servicesSLO.content.decode('utf-8')
+    try:
+        servicesSLO = requests.get(APMServicesURL)
+        if servicesSLO.status_code == 200:
+            return servicesSLO.content.decode('utf-8')
+    except Exception as e:
+        print(e)
 
 def getObjectIDs(orbusObjects):
     objectIDs = []
-    for objects in orbusObjects['value']:
-        objectIDs.append((objects['ObjectId'], objects['Name']))
-    return objectIDs
+    try:
+        for objects in orbusObjects['value']:
+            objectIDs.append((objects['ObjectId'], objects['Name']))
+        return objectIDs
+    except Exception as e:
+        print(e)
 
 def getServiceName(serviceObject):
     objects = {}
-    for key, obj in serviceObject.items():
-        for vals in obj:
-            if 'errorSpanCount' in vals:
-                objects[vals['series'][0]['value']['s']] = []
-                objects[vals['series'][0]['value']['s']].append(int(vals['errorSpanCount'])/int(vals['spanCount'])*100)
-    return objects
+    try:
+        for key, obj in serviceObject.items():
+            for vals in obj:
+                if 'errorSpanCount' in vals:
+                    objects[vals['series'][0]['value']['s']] = []
+                    objects[vals['series'][0]['value']['s']].append(int(vals['errorSpanCount'])/int(vals['spanCount'])*100)
+        return objects
+    except Exception as e:
+        print(e)
 
-#TODO: 1. Process Data gotten from APM and Orbus then Patch to Orbus with the patchObject(objectID) function
-# 2. API Authentication for both Orbus and APM -  Add Redirect URL for the server this code will be hosted on on Azure AD and grant permissions
+#TODO:
+# 1. API Authentication for both Orbus and APM -  Add Redirect URL for the server this code will be hosted on on Azure AD and grant permissions
 
 if __name__ == "__main__":
-    objectIDs = getObjectIDs(json.loads(getObjects()))
-    service_names = getServiceName(json.loads(getServicesSLO()))
+    try:
+        objectIDs = getObjectIDs(json.loads(getObjects()))
+        service_names = getServiceName(json.loads(getServicesSLO()))
 
-    for items in objectIDs:
-        print(items[0])
-        if items[1] in service_names:
-            service_names[items[1]].append(items[0])
+        for items in objectIDs:
+            if items[1] in service_names:
+                service_names[items[1]].append(items[0])
 
-    for key, val in service_names.items():
-        print(patchObject(val[1], val[0]))
+        for key, val in service_names.items():
+            patchObject(val[1], val[0])
+    except Exception as e:
+        print(e)
